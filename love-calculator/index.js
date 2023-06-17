b6316a899c4fb3da13af3b463766fa2cca998787
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-17 22:17:58 +0300

    Love_Calculator
    ı used if else and ı made love calculator.

---
+++ love-calculator/index.js
@@ -0,0 +1,14 @@
+prompt("What İs Your Name");
+prompt("What İs Their Name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore);
+
+if(loveScore > 70){
+    alert("Your love score is"+ loveScore + "%" + "You love each other like Ferhat and Şirin");
+}
+else {
+    alert("Your love score is"+ loveScore + "%");
+}
+
+alert("Your love score is"+ loveScore + "%");
